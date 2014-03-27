05326f6228ecaca84f30c7f29f3bb1631754b683
matthew.gerring <matthew.gerring@gmail.com>
2014-03-27 15:04:36 +0000

    Upgraded the windows fabio to 1.1

--- org.dawb.fable.feature/python/fabio_0.0.7/win32-x86/fabio/__init__.py
+++
@@ -1,9 +0,0 @@
-version = "0.0.7"
-
-import fabioimage
-import openimage
-from fabioutils import filename_object, COMPRESSORS, jump_filename, \
-        previous_filename, next_filename, deconstruct_filename, \
-        extract_filenumber, getnum
-from openimage import openimage as open
-
